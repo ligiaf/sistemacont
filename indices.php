4da197e07d12645272cc7f37160256ed7e352bf7
SamanthaAquino <samantha_aquino@outlook.com>
2017-06-19 22:48:51 -0300

    nova aba

--- indices.php
+++ indices.php
@@ -108,10 +108,10 @@ if (isset($_POST['envia_dado'])) {
                         <a href="indices.php" class="active"><i class="fa fa-table nav_icon"></i>Calcular Índices</a>
                     </li>
                     <li>
-                        <a href="graficos.html"><i class="fa fa-bar-chart nav_icon"></i>Gráficos</a>
+                        <a href="graficos.html" target="_blank"><i class="fa fa-bar-chart nav_icon"></i>Gráficos</a>
                     </li>
                     <li>
-                        <a href="sobre.html" ><i class="fa fa-file-text-o nav_icon"></i>Sobre</a>
+                        <a href="sobre.html" target="_blank"><i class="fa fa-file-text-o nav_icon"></i>Sobre</a>
                     </li>
                 </ul>
                 <div class="clearfix"> </div>
